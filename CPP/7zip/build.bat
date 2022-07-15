e1a7ad72ac3764e48634380fb497833e063cbbc9
Mike Seese <seesemichaelj@gmail.com>
2022-07-14 19:07:04 -0700

    get the vscode task working properly

---
+++ CPP/7zip/build.bat
@@ -0,0 +1,2 @@
+@REM Change the below URI to reflect 64bit or 32bit and your version of visual studio
+"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" & nmake NEW_COMPILER=1 MY_STATIC_LINK=1
